f2d40c7c680ba62402a4e9dfb2df4e1d17161ab6
Ajith <Abhay.Ajith@elektrobit.com>
2024-02-11 22:23:49 +0530

    First COmmit

--- app/src/main/java/com/example/order_app/view/PastryAdapter.java
+++ app/src/main/java/com/example/order_app/view/PastryAdapter.java
@@ -68,9 +68,6 @@ public class PastryAdapter extends RecyclerView.Adapter<PastryAdapter.ViewHolder
     }
 
     private void displayQuantity(TextView quantityTxt) {
-    }
-
-    private void displayQuantity(TextView quantityTxt) {
         quantityTxt.setText(String.valueOf(quantity));
     }
 
